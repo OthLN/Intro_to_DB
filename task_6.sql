e3138fd291feb9ccf09e1ee985e8fcac45589dc1
OthLN <ln.othmane@gmail.com>
2024-07-28 01:36:38 +0100

    Add files via upload

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,38 @@
--- Use the alx_book_store database
-USE alx_book_store;
+# task_6.py
 
--- Insert multiple rows into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+def generate_insert_statements():
+    # Define the data
+    customers = [
+        {
+            'customer_id': 2,
+            'customer_name': 'Blessing Malik',
+            'email': 'bmalik@sandtech.com',
+            'address': '124 Happiness Ave.'
+        },
+        {
+            'customer_id': 3,
+            'customer_name': 'Obed Ehoneah',
+            'email': 'eobed@sandtech.com',
+            'address': '125 Happiness Ave.'
+        },
+        {
+            'customer_id': 4,
+            'customer_name': 'Nehemial Kamolu',
+            'email': 'nkamolu@sandtech.com',
+            'address': '126 Happiness Ave.'
+        }
+    ]
+
+    # Generate SQL statements
+    sql_statements = []
+    for customer in customers:
+        sql = f"INSERT INTO customer (customer_id, customer_name, email, address) VALUES ({customer['customer_id']}, '{customer['customer_name']}', '{customer['email']}', '{customer['address']}');"
+        sql_statements.append(sql)
+
+    return sql_statements
+
+if __name__ == '__main__':
+    # Print the generated SQL statements
+    statements = generate_insert_statements()
+    for statement in statements:
+        print(statement)
